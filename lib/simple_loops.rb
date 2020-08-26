14b975b3871cadcce927d9855d1fcff53417c8c6
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 19:05:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -55,12 +55,3 @@ def return_string_array (array)
     count += 1 
   end
 end
-
-///
-describe 'output_array' do
-
-  it 'takes in a message and a number outputs the message that number of times' do
-    array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
-    expect { output_array(array) }.to output(/Be yourself;\neveryone else is already taken\n- Oscar Wilde/).to_stdout, "Expected:\n\nBe yourself;\neveryone else is already taken\n- Oscar Wilde"
-   array = [5, 4, 3, 2, 1]
-    expect { output_array(array) }.to output(/5\n4\n3\n2\n1\n/).to_stdout
