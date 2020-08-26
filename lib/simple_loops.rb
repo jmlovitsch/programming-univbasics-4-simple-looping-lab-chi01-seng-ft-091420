43d53edb4ab599d176d7e214a59f741c8046a3a1
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 19:05:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -58,12 +58,3 @@ describe 'output_array' do
     expect { output_array(array) }.to output(/Be yourself;\neveryone else is already taken\n- Oscar Wilde/).to_stdout, "Expected:\n\nBe yourself;\neveryone else is already taken\n- Oscar Wilde"
     array = [5, 4, 3, 2, 1]
     expect { output_array(array) }.to output(/5\n4\n3\n2\n1\n/).to_stdout
-  end
-end
-
-describe 'return_string_array' do
-  it 'takes in an array of elements, converts them to strings and returns them all in a new array' do
-    array = [5, 4, 3, 2, 1]
-    expect(return_string_array(array)).to eq(["5", "4", "3", "2", "1"])
-  end
-end
