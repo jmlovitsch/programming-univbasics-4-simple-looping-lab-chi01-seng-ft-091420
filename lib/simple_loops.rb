f1dc2d07402c0f5c5b41ae91236fb48844016636
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 18:54:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -33,7 +33,11 @@ def loop_message_n_times (string, integer)
 end
 
 def output_array
-  
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end
 end
 
 def return_string_array
