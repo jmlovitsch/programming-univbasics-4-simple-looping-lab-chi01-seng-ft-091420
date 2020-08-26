914cd8b0db3bd48c97973f969e12e2b16907b521
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 19:04:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -41,10 +41,28 @@ def output_array(array)
   end
 end
 
-def return_string_array
+def return_string_array (array)
   count = 0 
   while count < array.length do
     
     count += 1 
+  end
+end
+
+///
+describe 'output_array' do
+
+  it 'takes in a message and a number outputs the message that number of times' do
+    array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+    expect { output_array(array) }.to output(/Be yourself;\neveryone else is already taken\n- Oscar Wilde/).to_stdout, "Expected:\n\nBe yourself;\neveryone else is already taken\n- Oscar Wilde"
+    array = [5, 4, 3, 2, 1]
+    expect { output_array(array) }.to output(/5\n4\n3\n2\n1\n/).to_stdout
+  end
+end
+
+describe 'return_string_array' do
+  it 'takes in an array of elements, converts them to strings and returns them all in a new array' do
+    array = [5, 4, 3, 2, 1]
+    expect(return_string_array(array)).to eq(["5", "4", "3", "2", "1"])
   end
 end
