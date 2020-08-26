b9ab3fedc691428ac2535dac3d9177aff9eb6a70
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 18:46:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
-# Write your methods here
+def loop_message_five_times (string)
+  count = 0 
+  message = "Hello World."
+  while count <5 do
+    puts message
+    count += 1 
+  end
+end
+
+def loop_message_n_times (string, integer)
+  message = "Hello Moon."
+  number = 5
+
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
